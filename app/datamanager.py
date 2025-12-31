7de480ca0af09123a8e05b054c138048ac7590e5
Pumklns <adamlat190@gmail.com>
2025-12-31 13:24:05 +0100

    Ok classe pour json genre ya fonction ecrire lire modifier tt ca j'ai pas mit de commentaires mais vas-y c tres clair il me semble tt ca c pour mettre en place un reveil fonctionnel

---
+++ app/datamanager.py
@@ -0,0 +1,36 @@
+import json 
+import os
+
+
+class DataManager:
+    def __init__(self, dirbase, filename="reveil.json"):
+        self.path = os.path.join(dirbase, filename)
+        
+    def look_state(self):
+        return os.path.exists(self.path)   
+    
+    
+    def write(self,data):
+        
+        try:    
+            with open(self.path,'w') as f:
+                json.dump(data,f, indent=4)
+        except Exception as e:
+            print(f"Bug ecriture json sur ce chemin {self.path} Erreur : {e}")
+            return {}
+        
+    def read(self):
+        if not self.look_state():
+            return {}
+        try:
+            with open(self.path,'r') as f:
+                data =  json.load(f)
+        except Exception as e:
+            print(f"Bug lecture json sur ce chemin {self.path} Erreur : {e}")
+            return {}
+        return data
+    
+    def change(self,id_alarm,new_data):
+        all_data = self.read()
+        all_data[id_alarm] = new_data
+        self.write(all_data)
